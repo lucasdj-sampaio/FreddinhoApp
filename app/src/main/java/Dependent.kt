28a14333cd1391f3539a9c82d906603637c009fe
Lucas de Jesus Sampaio <lucas.jesus@gpnet.com.br>
2022-11-10 22:18:36 -0300

    Rollback

---
+++ app/src/main/java/Dependent.kt
@@ -0,0 +1,7 @@
+data class Dependent (
+    val id: Int,
+    val name: String,
+    val gender: Char,
+    val birthYear: Int,
+    val accountModelId: Int
+)
--- app/src/main/java/com/fenix/freddinho/MainActivity.kt
+++ app/src/main/java/com/fenix/freddinho/MainActivity.kt
@@ -1,93 +1,28 @@
 package com.fenix.freddinho
+
 import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import okhttp3.*
-import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
     private lateinit var dialog: AlertDialog
-    private val client = OkHttpClient()
-    private val mapper = jacksonObjectMapper()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        val preferences = getPreferences(MODE_PRIVATE);
-
-        try {
-            val userName = preferences.getString("userName", "").toString()
-            val password = preferences.getString("userPass", "").toString()
-
-            var userId = getUserId(userName, password)
-
-            var dependents = getDependentByUserId(userId)
-
-            setContentView(R.layout.activity_main)
-
-            findViewById<Button>(R.id.btn_profile_girl).setOnClickListener {
-                showDialogNormal()
-            }
-
-            val btnProfileBoy = findViewById<Button>(R.id.btn_profile_boy)
-
-            btnProfileBoy.setOnClickListener {
-                val a = Intent(this, ChatChild::class.java)
-                startActivity(a)
-            }
-        }
-        catch (e: Exception) {
-            Toast.makeText(applicationContext,
-                    "Falha ao realizar login: ${e.message}",
-                    Toast.LENGTH_LONG)
-
-            setContentView(R.layout.activity_login)
-        }
-    }
-
-    private fun getUserId(user: String, password: String): String{
-        val request = Request.Builder()
-                .url("https://freddinho-api.azurewebsites.net/validcredential")
-                .header("password", password)
-                .header("email", user)
-                .build()
-        var requestResponse = ""
-
-        client.newCall(request).enqueue(object : Callback{
-            override fun onFailure(call: Call, e: IOException) {
-                requestResponse = e.message.toString()
-            }
-
-            override fun onResponse(call: Call, response: Response) {
-                requestResponse = response.body()?.string().toString()
-            }
-        })
-
-        return requestResponse;
-    }
-
-    private fun getDependentByUserId(userId: String): List<Dependent>?{
-        val dependentList: List<Dependent>
-
-        try {
-            val request = Request.Builder()
-                    .url("https://freddinho-api.azurewebsites.net/validcredential")
-                    .header("userId", userId)
-                    .build()
-            val response = client.newCall(request).execute()
-
-            dependentList = mapper.readValue(response.body().toString()
-                    , List::class.java) as List<Dependent>
-        }
-        catch (e: Exception){
-            return null
+        findViewById<Button>(R.id.btn_profile_girl).setOnClickListener {
+            showDialogNormal()
         }
 
-        return dependentList
+        val btnProfileBoy = findViewById<Button>(R.id.btn_profile_boy)
+
+        btnProfileBoy.setOnClickListener {
+            val a = Intent(this, ChatChild::class.java)
+            startActivity(a)
+        }
     }
 
     private fun showDialogNormal(){
