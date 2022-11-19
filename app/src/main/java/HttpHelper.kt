57cdb97f23dee89fa5b65a52678403106efad28a
Lucas de Jesus Sampaio <lucas.jesus@gpnet.com.br>
2022-11-19 02:45:27 -0300

    Trade some ref about endpoint

--- app/src/main/java/HttpHelper.kt
+++ app/src/main/java/HttpHelper.kt
@@ -1,17 +1,24 @@
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
+import okhttp3.MediaType
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import okhttp3.RequestBody
 
 class HttpHelper {
 
     fun getUserId(email: String, password: String): String{
         val client = OkHttpClient()
 
+        val body = RequestBody
+                .create(MediaType.parse("application/json; charset=utf-8"), "{\n" +
+                        "    \"email\": \"${email}\",\n" +
+                        "    \"password\": \"${password}\"\n" +
+                        "}")
+
         val request = Request.Builder()
                 .url("https://freddinho-api.azurewebsites.net/getaccountid")
-                .header("password", password)
-                .header("email", email)
+                .post(body)
                 .build()
 
         val exec = client.newCall(request).execute();
@@ -26,8 +33,7 @@ class HttpHelper {
 
         try {
             val request = Request.Builder()
-                    .url("https://freddinho-api.azurewebsites.net/getdependent")
-                    .header("userId", userId)
+                    .url("https://freddinho-api.azurewebsites.net/getdependent?userid=${userId}")
                     .get()
                     .build()
             val response = client.newCall(request).execute()
